6c8019afa5b9f4caf07075d2122b2972830766df
Giroun <dannnyk605@gmail.com>
2024-09-26 23:18:09 -0700

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+function Card(){
+  return (
+    <div className="card">
+      <div className="favorite">
+        <img src="/img/heart-unliked.svg" alt="unliked" />
+      </div>
+      <img width={133} height={112} src="/img/sneackers/1.jpg" alt="Sneackers" />
+      <h5>Мужские Кроссовки Under Armour Curry 8</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column ">
+          <span>Цена:</span>
+          <b>12 999 руб.</b>
+        </div>
+        <button className="button">
+          <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Card;
