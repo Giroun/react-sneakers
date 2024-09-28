af07a0162fd9b36eeb998a789df930b6ad10037a
Giroun <dannnyk605@gmail.com>
2024-09-28 14:39:00 -0700

    add card structure and modified React logic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import Card from'./components/Card';
 import Header from'./components/Header';
 import Drawer from './components/Drawer';
 
+const array = [
+  {
+    title: 'Мужские Кроссовки Under Armour Curry 8', 
+    price: 12990, 
+    imageUrl: './img/sneackers/1.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 9990, 
+    imageUrl: './img/sneackers/2.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 15990, 
+    imageUrl: './img/sneackers/3.jpg'
+  },
+]
+
 function App() {
   return (
     <div className="wrapper clear">
-        <Drawer />
+      <Drawer />
       <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
@@ -16,7 +34,15 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card />
+          {
+          array.map((obj) => (
+            <Card 
+            title={obj.title} 
+            price={obj.price}
+            imageUrl={obj.imageUrl}
+            onClick={() => console.log(obj.price)}
+            />
+          ))}
         </div>
       </div>
     </div>
